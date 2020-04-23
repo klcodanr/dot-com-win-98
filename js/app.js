743a996cc4c75ddf01bdd08fd4b4052ab23e0259
klcodanr <klcodanr@users.noreply.github.com>
2020-04-23 21:32:19 +0000

    Deploying to gh-pages from  @ d28f27a31fa0d3116c47f2c1a79216ce5f9d94e9 🚀

--- js/app.js
+++ js/app.js
@@ -1721,25 +1721,31 @@ document.querySelectorAll(".window").forEach((win) => {
     grid: 10,
     handle: win.querySelector(".title-bar"),
   });
+  win.style.height="50vh";
+  win.style.width="50vw";
+  win.style.margin="5em";
 });
 
-new MutationObserver(function(mutations) {
-    mutations.some(function(mutation) {
-      if (mutation.type === 'attributes' && mutation.attributeName === 'src') {
-        console.log(mutation);
-        console.log('Old src: ', mutation.oldValue);
-        console.log('New src: ', mutation.target.src);
-        return true;
-      }
-  
-      return false;
-    });
-  }).observe(document.querySelector(".web-frame"), {
-    attributes: true,
-    attributeFilter: ['src'],
-    attributeOldValue: true,
-    characterData: false,
-    characterDataOldValue: false,
-    childList: false,
-    subtree: true
+
+document.querySelector("main").classList.remove("is-hidden");
+
+new MutationObserver(function (mutations) {
+  mutations.some(function (mutation) {
+    if (mutation.type === "attributes" && mutation.attributeName === "src") {
+      console.log(mutation);
+      console.log("Old src: ", mutation.oldValue);
+      console.log("New src: ", mutation.target.src);
+      return true;
+    }
+
+    return false;
   });
+}).observe(document.querySelector(".web-frame"), {
+  attributes: true,
+  attributeFilter: ["src"],
+  attributeOldValue: true,
+  characterData: false,
+  characterDataOldValue: false,
+  childList: false,
+  subtree: true,
+});
